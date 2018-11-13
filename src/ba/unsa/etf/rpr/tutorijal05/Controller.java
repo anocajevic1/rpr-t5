354ca375ec95c3a474b2f9e9d2b025df58fa8314
anocajevic1 <anocajevic1@etf.unsa.ba>
2018-11-13 14:46:03 +0100

    sredeno onAction povezivanje

--- src/ba/unsa/etf/rpr/tutorijal05/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Controller.java
@@ -1,5 +1,23 @@
 package ba.unsa.etf.rpr.tutorijal05;
 
+import javafx.event.ActionEvent;
+
 public class Controller {
+
+    public void multiply(ActionEvent actionEvent) {
+
+    }
+
+    public void substract(ActionEvent actionEvent) {
+    }
+
+    public void add(ActionEvent actionEvent) {
+    }
+
+    public void module(ActionEvent actionEvent) {
+    }
+
+    public void divide(ActionEvent actionEvent) {
+    }
 
 }

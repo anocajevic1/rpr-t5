0bfdd9cb702845031a1d46d4054c514b26d5d528
anocajevic1 <anocajevic1@etf.unsa.ba>
2018-11-13 14:55:33 +0100

    urađena implementacija za evente

--- src/ba/unsa/etf/rpr/tutorijal05/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Controller.java
@@ -4,20 +4,4 @@ import javafx.event.ActionEvent;
 
 public class Controller {
 
-    public void multiply(ActionEvent actionEvent) {
-
-    }
-
-    public void substract(ActionEvent actionEvent) {
-    }
-
-    public void add(ActionEvent actionEvent) {
-    }
-
-    public void module(ActionEvent actionEvent) {
-    }
-
-    public void divide(ActionEvent actionEvent) {
-    }
-
 }
--- src/ba/unsa/etf/rpr/tutorijal05/Main.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Main.java
@@ -1,26 +1,82 @@
 package ba.unsa.etf.rpr.tutorijal05;
 
+import java.math.BigDecimal;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+public final class Main extends Application {
 
-public class Main extends Application {
+    private BigDecimal left;
+    private String selectedOperator;
+    private boolean numberInputting;
+
+    @FXML
+    private TextField display;
+
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("digitron.fxml"));
-        primaryStage.setTitle("Digitron");
-        primaryStage.setScene(new Scene(root, 200, 275));
-        primaryStage.show();
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Calculator");
+        stage.setOnCloseRequest(x -> {
+            Platform.exit();
+        });
+        stage.setResizable(false);
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("digitron.fxml"))));
+        stage.show();
+    }
+
+    @FXML
+    protected void handleOnAnyButtonClicked(ActionEvent evt) {
+        Button button = (Button)evt.getSource(); // koja je tipka
+        final String buttonText = button.getText();
+
+        if (buttonText.matches("[0-9\\.]")) {
+            if (!numberInputting) {
+                numberInputting = true;
+                display.clear();
+            }
+            display.appendText(buttonText);
+            return;
+        }
+        if (buttonText.matches("[＋－×/%]")) {
+            left = new BigDecimal(display.getText());
+            selectedOperator = buttonText;
+            numberInputting = false;
+            return;
+        }
+        if (buttonText.equals("=")) {
+            final BigDecimal right = numberInputting ? new BigDecimal(display.getText()) : left;
+            left = calculate(selectedOperator, left, right);
+            display.setText(left.toString());
+            numberInputting = false;
+            return;
+        }
+    }
+
+    static BigDecimal calculate(String operator, BigDecimal left, BigDecimal right) {
+        switch (operator) {
+            case "＋":
+                return left.add(right);
+            case "－":
+                return left.subtract(right);
+            case "×":
+                return left.multiply(right);
+            case "÷":
+                return left.divide(right);
+            default:
+        }
+        return right;
     }
 
     public static void main(String[] args) {
         launch(args);
     }
-
-
 
 }
